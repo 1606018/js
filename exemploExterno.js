d733d1dee400876d0804c86b09a1160aaf81a3b2
SabinoWS <sabino.th@hotmail.com>
2022-09-11 20:01:32 -0300

    Ajustes aula JS

--- exemploExterno.js
+++ exemploExterno.js
@@ -1,8 +1,8 @@
 // Pegando botao do HTML
-const meuBotao_1 = document.getElementById("incluirExterno")
+const elementoBotaoArquivo = document.getElementById("incluirExterno")
 
 // Adicionando um evento ao Botão
-meuBotao_1.addEventListener("click", dispararAlert)
+elementoBotaoArquivo.addEventListener("click", dispararAlert)
 
 // Função efetuando algo
 function dispararAlert() {
